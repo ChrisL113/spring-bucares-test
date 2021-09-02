c4f69d791604b87478d730686522fc34707994c1
KristoLopez <galileo.20@hotmail.com>
2021-09-02 14:44:04 -0500

    first commit

---
+++ src/test/java/com/example/nubedianTest/NubedianTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nubedianTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubedianTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
